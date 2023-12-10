34bd80f1de96b3a1eb4597c4365b12d451adda0f
Somil Kaushik <somil.kaushik@somilkss297.successivetech.com>
2023-12-10 14:37:24 +0530

    Assignment-9

---
+++ Assignment-9/Ques2/Ques2.js
@@ -0,0 +1,7 @@
+const lodash = require("lodash");
+
+let sumOfNumber = lodash.add(2, 5);
+console.log(sumOfNumber);
+
+let minimumValue = lodash.min([5, 3, 4, 1, 7]);
+console.group(minimumValue);
---
+++ Assignment-9/Ques3/index.js
@@ -0,0 +1,4 @@
+const {add, sub} = require("./math/math")
+
+console.log(add(3, 6));
+console.log(sub(6, 9));
